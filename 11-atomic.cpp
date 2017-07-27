e9048dc4e632b7091a6034544aeb21aae7b63793
Ilya Kaliman <ilya.kaliman@gmail.com>
2017-07-27 11:28:04 -0700

    Use more relevant example.

--- 11-atomic.cpp
+++ 11-atomic.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
 
+double
+two_body_energy(int i, int j)
+{
+	return (2.0 * i + 3.0 * j) / 10.0; // some dummy return value
+}
+
 int
 main()
 {
-	double sum = 0.0; // shared by default
+	const int nbodies = 1000;
+	double energy = 0.0;
 
-#pragma omp parallel for
-	for (int i = 0; i < 10000; i++) {
-		sum += i;
+	for (int i = 0; i < nbodies; i++) {
+		for (int j = i+1; j < nbodies; j++) {
+			double eij = two_body_energy(i, j);
+			energy += eij;
+		}
 	}
 
-	std::cout << "sum = " << sum << std::endl;
+	std::cout << "energy = " << energy << std::endl;
+	return 0;
 }
 
-// XXX: why the sum is incorrect?
-// XXX: make addition atomic (omp atomic) to fix the bug
+// XXX: parallelize the outer loop
+// XXX: why is the result incorrect?
+// XXX: use omp atomic to fix the bug
--- 12-reduction.cpp
+++ 12-reduction.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
 
+double
+two_body_energy(int i, int j)
+{
+	return (2.0 * i + 3.0 * j) / 10.0; // some dummy return value
+}
+
 int
 main()
 {
-	double sum = 0.0;
+	const int nbodies = 1000;
+	double energy = 0.0;
 
-#pragma omp parallel for reduction(+:sum)
-	for (int i = 0; i < 10000; i++)
-		sum += i;
+#pragma omp parallel for reduction(+:energy)
+	for (int i = 0; i < nbodies; i++) {
+		for (int j = i+1; j < nbodies; j++) {
+			double eij = two_body_energy(i, j);
+			energy += eij;
+		}
+	}
 
-	std::cout << "sum = " << sum << std::endl;
+	std::cout << "energy = " << energy << std::endl;
 	return 0;
 }
 
-// XXX: there are other reduction operations
+// XXX: there are other reduction operations (* - && || max min ...)
 // XXX: reduction takes care of the scope
-// XXX: what happens when we try to make 'sum' variable shared
+// XXX: what happens when we change the initial value of energy?
+// XXX: what happens when we try to make energy "shared"?
